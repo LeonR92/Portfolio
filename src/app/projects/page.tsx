1d635a963057f9f062a05ed41f6bed91d3c3b898
LeonR <leonjy92@gmail.com>
2025-03-14 14:12:10 +0100

    added title and desc in /projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -42,9 +42,8 @@ function Article({ article }: { article: ArticleWithSlug }) {
 }
 
 export const metadata: Metadata = {
-  title: 'Articles',
-  description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+  title: 'Meine Entwicklungsreise',
+  description: 'Projekte aus meinem beruflichen Werdegang',
 }
 
 export default async function ArticlesIndex() {
@@ -52,8 +51,8 @@ export default async function ArticlesIndex() {
 
   return (
     <SimpleLayout
-      title="Writing on software design, company building, and the aerospace industry."
-      intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+      title="Meine Entwicklungsreise"
+      intro="Eine Github-Sammlung meiner Projekte, die meinen Entwicklungsweg und meine Herangehensweise an Softwareentwicklung widerspiegeln. Hier finden Sie Lösungen, die ich bei meinen Arbeitgebern umgesetzt habe."
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
