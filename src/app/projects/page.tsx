2f4e543c26c0ba75063c4e93a1c77426adbae4d6
LeonR <leonjy92@gmail.com>
2025-03-14 22:58:01 +0100

    removed link in personal portfolio project article

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -52,7 +52,7 @@ export default async function ArticlesIndex() {
   return (
     <SimpleLayout
       title="Meine Entwicklungsreise"
-      intro="Eine Github-Sammlung meiner Projekte, die meinen Entwicklungsweg und meine Herangehensweise an Softwareentwicklung widerspiegeln. Hier findest Du Lösungen, die ich bei meinen Arbeitgebern umgesetzt habe."
+      intro="Projekte, die meinen Entwicklungsweg und meine Herangehensweise an Softwareentwicklung widerspiegeln. Hier findest Du Lösungen, die ich bei meinen Arbeitgebern umgesetzt habe."
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
